a98843bc60f86efda72fc28fde42bc96dad9b845
Nestor Júnior <nestorjunior@MacBook-Pro-de-Nestor.local>
2020-02-26 05:15:13 -0300

    create method init

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,17 @@ class CalcController {
     constructor(){
         this._displayCalc = '0';
         this._currentDate;
+        this.init();
+    }
+
+    init() {
+        let displayEl = document.querySelector('#display');
+        let dateEl = document.querySelector('#data');
+        let timeEl = document.querySelector('#hora');
+
+        displayEl.innerHTML = '123';
+        dateEl.innerHTML = '10/10/2020';
+        timeEl.innerHTML = '13:40';
     }
 
     get displayCal() {

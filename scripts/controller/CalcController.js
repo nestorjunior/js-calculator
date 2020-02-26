595bd2100c327956138b7da45b0328cf6aa46571
Nestor Júnior <nestorjunior@MacBook-Pro-de-Nestor.local>
2020-02-26 04:51:58 -0300

    create get and set atribute

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,3 +1,22 @@
-    class CalcController {
-        
+class CalcController {
+    constructor(){
+        this._displayCalc = '0';
+        this._currentDate;
     }
+
+    get displayCal() {
+        return this._displayCalc;
+    }
+
+    set displayCal(value) {
+        this._displayCalc = value;
+    }
+
+    get currentDate() {
+        return this._currentDate;
+    }
+
+    set currentDate(value) {
+        return this._currentDate = value;
+    }
+}
